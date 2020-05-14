3883e7b814d0e69d7c3b75f185bd28001ed93d72
Soslan <sparksoslan@gmail.com>
2020-05-14 14:03:15 +0100

    Add Chart and fill with data

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,13 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { fetchDailyData } from '../../API';
+import { Line, Bar } from 'react-chartjs-2';
+import styles from './Chart.module.css';
 
 const Chart = () => {
-  const [dailyData, setDailyData] = useState({});
+  const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
     (async () => setDailyData(await fetchDailyData()))();
   }, [setDailyData]);
-  return <h1>Chart</h1>;
+
+  const lineChart = dailyData.length ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true,
+          },
+          {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255,0,0,0.5',
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{lineChart}</div>;
 };
 
 export default Chart;
